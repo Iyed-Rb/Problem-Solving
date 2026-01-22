5d979701fa7055b6e7ed3c202b1073091d30cea3
Iyed <rabiaiyed743@gmail.com>
2026-01-23 00:36:40 +0100

    add some dictionary problem

---
+++ DataStructures/DH-Problem 07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> dataEntries = new HashSet<string>();
+
+        string[] entries = { "A", "B", "C", "A" }; // A is a duplicate
+        foreach (var entry in entries)
+        {
+            if (!dataEntries.Add(entry))
+            {
+                Console.WriteLine($"Duplicate detected: {entry}");
+            }
+        }
+        Console.ReadKey();
+
+        // Output: Duplicate detected: A
+    }
+}
---
+++ DataStructures/DH-Problem 11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int Count(int[] arr)
+    {
+        int count = 0;
+        if (arr.Length > 0) count++;
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i + 1] - arr[i] == 1) 
+                count++;
+            else
+                return count;
+        }
+
+
+        return count;
+    }
+
+    static void Main()
+    {
+        int[] arr = { 100, 4, 200, 1, 3, 2 };
+        Array.Sort(arr);
+
+        Console.WriteLine("Long Consecutive Sequence: " + Count(arr));
+
+
+    }
+}
